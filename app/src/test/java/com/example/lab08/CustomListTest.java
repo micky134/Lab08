908427ed7f23a5c0669ffbe124c80fa8ffdc3ed2
micky134 <michali@ualberta.ca>
2026-03-05 17:47:44 -0700

    Update CustomListTest.java

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -3,5 +3,8 @@ package com.example.lab08;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class CustomListTest {
+  public boolean hasCity(City city) {
+  return cities.contains(city);
+  }
 
 }
