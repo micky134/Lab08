f5434e0af23150751b290e80fae5f81a66a08474
micky134 <michali@ualberta.ca>
2026-03-06 13:49:02 -0700

    Rename CustomListTest to CityListTest

--- app/src/test/java/com/example/lab08/CityListTest.java
+++ app/src/test/java/com/example/lab08/CityListTest.java
@@ -2,7 +2,7 @@ package com.example.lab08;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-public class CustomListTest {
+public class CityListTest {
   public boolean hasCity(City city) {
   return cities.contains(city);
   }
